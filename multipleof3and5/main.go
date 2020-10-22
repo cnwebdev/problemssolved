bb9a598827c5a463345ad53119fbb5753bc1fd68
Chris Nguyen <cn@mbp.local>
2020-10-22 00:30:12 -0700

    add comment to code

--- multipleof3and5/main.go
+++ multipleof3and5/main.go
@@ -8,19 +8,25 @@ package main
 import "fmt"
 
 func main() {
+
+	// slice to hold multiples of 3 and 5 in 1 to 1000
 	multiples3n5 := []int{}
 
+	// find multiple of 3 and 5, then append to slice multiples3n5
 	for i := 1; i < 1000; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			multiples3n5 = append(multiples3n5, i)
 		}
 	}
 
+	// Heading text
 	fmt.Println("Multiples of 3 an 5 in 1 to 1000 are:")
+
+	// Calculate sum from slice of multiple of 3 and 5 in 1 to 1000
 	sum := 0
 	for i, n := range multiples3n5 {
 		fmt.Printf("#%-5d %d\n", i, n)
 		sum += n
 	}
-	fmt.Printf("The sume of multiple of 3 and 5 in 1 to 1000 is %d\n", sum)
+	fmt.Printf("The sum of multiple of 3 and 5 in 1 to 1000 is %d\n", sum)
 }
